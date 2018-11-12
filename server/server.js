cfe9e9b8b05a8cea9c3961e83a4136f21e411960
Edfil Basan <edgdulay@gmail.com>
2018-11-11 16:02:06 -0800

    server updates

--- server/server.js
+++ server/server.js
@@ -5,6 +5,15 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('this is working.')
 })
+
 app.listen(3000, () => {
     console.log('app is running biatch 3000')
 })
+
+
+// TO-DO
+// res = this is working
+// /signin -> post = success/fail
+// /register - > post = user
+// /profile/userID -> get = user
+// /image -> PUT -> user
